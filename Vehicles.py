16ea5fbe0e8b23234c84ff5a40f76787a52419b6
Alex <alextr86@gmail.com>
2023-04-13 01:43:07 +0300

    Added menu

--- Vehicles.py
+++ Vehicles.py
@@ -1,8 +1,22 @@
+import os
+from time import *
 from random import randint
+
+# Menu list
+menu = ("Drive vehicle", "Add wheel to vehicle", "Remove wheel from vehicle", "Inflate wheel",
+        "Puncture wheel", "Repair punctured wheel", "Quit")
+
+
+# clear the terminal screen
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 class Wheel:
+    existing_wheels_count = 0
+
     def __init__(self, wheel_radius, set_air_press=-1):
+        Wheel.existing_wheels_count += 1
         self.puncture = False
         self.radius = wheel_radius
         self.air_percentage = set_air_press if set_air_press != -1 else randint(85, 95)
@@ -12,7 +26,7 @@ class Wheel:
 
 
 class Vehicle:
-    def __init__(self, wheels_lst):
+    def __init__(self, wheels_lst=[]):
         self.wheels_list = wheels_lst
         self.driveable = self.set_driveable()
 
@@ -28,26 +42,101 @@ class Vehicle:
             for w in self.wheels_list:
                 w.air_percentage -= dist
             self.driveable = self.set_driveable()
+            print(f"Vehicle driven successfully {dist} KM!")
         else:
-            print("Vehicle is not drivable! Fix problems and then try again")
+            if len(self.wheels_list) == 0:
+                print("Vehicle is not drivable! Vehicle can't drive without wheels!")
+            elif len(self.wheels_list) % 2 != 0:
+                print(f"Vehicle is not drivable!! {len(self.wheels_list)} is an illegal number of wheels!")
+            else:
+                print("Vehicle is not drivable! Fix problems and try again.")
 
     def set_driveable(self):
         is_driveable = True
-        if self.wheels_list is not None:
+        if (len(self.wheels_list) != 0) and (len(self.wheels_list) % 2 == 0):
             first_wheel_radius = self.wheels_list[0].radius
             for w in self.wheels_list:
                 if (w.radius != first_wheel_radius) or (w.air_percentage <= 60) or w.puncture:
                     is_driveable = False
                     break
+        else:
+            is_driveable = False
         return is_driveable
 
 
 if __name__ == '__main__':
-    Wh1 = Wheel(28, 75)
-    Wh2 = Wheel(28)
-    Veh1 = Vehicle([Wh1, Wh2])
-    Wh3 = Wheel(28)
-    Wh4 = Wheel(28, 70)
-    Veh1.add_del_wheel(Wh3, True)
-    Veh1.add_del_wheel(Wh4, True)
-    Veh1.drive(20)
+    veh = Vehicle()
+    while True:
+        clear()
+        i = 1
+        print("You are the proud owner of a vehicle!")
+        for m in menu:
+            print(f"{i}) {m}")
+            i = i + 1
+        ch = input("What do you want to do? Please enter your choice: ")
+
+        match ch:
+            case '1':
+                driving_dist = int(input("Please enter desired driving distance: "))
+                veh.drive(driving_dist)
+                sleep(2)
+            case '2':
+                input_wheel_radius = int(input("Please enter desired wheel radius: "))
+                veh.add_del_wheel(Wheel(input_wheel_radius), True)
+                print("Wheel was successfully added!")
+                sleep(2)
+            case '3':
+                tmp_wheel = int(input("Please enter No. of wheel to remove from vehicle: "))
+                if len(veh.wheels_list) != 0:
+                    if tmp_wheel <= len(veh.wheels_list):
+                        veh.add_del_wheel(veh.wheels_list[(tmp_wheel - 1)], False)
+                        print(f"Wheel No. {tmp_wheel} was successfully removed!")
+                    else:
+                        print(f"Error! Wheel No. {tmp_wheel} doesn't exist!")
+                else:
+                    print("Error! There are no wheels at all!")
+                sleep(2)
+            case '4':
+                tmp_wheel = int(input("Please enter No. of wheel to inflate: "))
+                if len(veh.wheels_list) != 0:
+                    if tmp_wheel <= len(veh.wheels_list):
+                        veh.wheels_list[(tmp_wheel - 1)].inflate()
+                        print(f"Wheel No. {tmp_wheel} was inflated!")
+                    else:
+                        print(f"Error! Wheel No. {tmp_wheel} doesn't exist!")
+                else:
+                    print("Error! There are no wheels at all!")
+                veh.driveable = veh.set_driveable()
+                sleep(2)
+            case '5':
+                tmp_wheel = int(input("Please enter No. of wheel to puncture: "))
+                if len(veh.wheels_list) != 0:
+                    if tmp_wheel <= len(veh.wheels_list):
+                        veh.wheels_list[(tmp_wheel - 1)].puncture = True
+                        print(f"Wheel No. {tmp_wheel} was punctured!")
+                    else:
+                        print(f"Error! Wheel No. {tmp_wheel} doesn't exist!")
+                else:
+                    print("Error! There are no wheels at all!")
+                veh.driveable = veh.set_driveable()
+                sleep(2)
+            case '6':
+                tmp_wheel = int(input("Please enter No. of punctured wheel to repair: "))
+                if len(veh.wheels_list) != 0:
+                    if tmp_wheel <= len(veh.wheels_list):
+                        veh.wheels_list[(tmp_wheel - 1)].puncture = False
+                        print(f"Wheel No. {tmp_wheel} was successfully repaired!")
+                    else:
+                        print(f"Error! Wheel No. {tmp_wheel} doesn't exist!")
+                else:
+                    print("Error! There are no wheels at all!")
+                veh.driveable = veh.set_driveable()
+                sleep(2)
+            case '7':
+                clear()
+                print("Goodbye!")
+                break
+            case _:
+                clear()
+                print("Wrong input!")
+                sleep(2)
